bd5ac840105dc90f8834303fd5894fd915304d13
Vishal Singh <vishalsingh2706@gmail.com>
2020-04-29 10:37:07 +0530

    add challenge 3 instructions

--- Sources/BrCommit/main.swift
+++ Sources/BrCommit/main.swift
@@ -1,1 +1,28 @@
+import ShellOut
 
+let arguments = CommandLine.arguments
+
+guard arguments.count > 1 else { fatalError("Please enter the commit message") }
+let originalCommitMessage = arguments[1]
+
+do {
+    let branchName = try shellOut(to: "git rev-parse --abbrev-ref HEAD")
+    print("You are currently working on: \(branchName)")
+    
+    // Challenge 3 --
+    //
+    // Create a string updatedMessage that combines the branch
+    // name with the original commit message provided by the user
+    //
+    // try, using shellout, git commit command
+    // for commiting with the updatedMessage
+    // **hint**: commit command is in this format:
+    // git commit -am "<message>"
+    //
+    // store the output of this command in a constant
+    // and print the output
+} catch {
+    let error = error as! ShellOutError
+    print(error.message) // Prints STDERR
+    print(error.output) // Prints STDOUT
+}
