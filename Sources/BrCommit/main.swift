9a9033238dcade8c9ef61409200e3d22d71b71d0
Vishal Singh <vishalsingh2706@gmail.com>
2020-04-29 12:01:33 +0530

    add instructions for part 2

--- Sources/BrCommit/main.swift
+++ Sources/BrCommit/main.swift
@@ -11,6 +11,8 @@ do {
     
     // Challenge 4 --
     //
+    // Part 1
+    //
     // Create a string updatedMessage that combines the branch
     // name with the original commit message provided by the user
     //
@@ -21,6 +23,27 @@ do {
     //
     // store the output of this command in a constant
     // and print the output
+
+    //
+    // Part 2
+    // Once you are successful with above
+    // export your executable target as a CLT (Command Line Tool)
+    // use the following commands from the root folder
+    //
+    // swift build -c release
+    // cd .build/release
+    // cp -f BrCommit /usr/local/bin/<name-of-your-choice>
+    //
+    // Once finished exporting (lets say, the name is br-commit)
+    //
+    // try this new CLT that you have created
+    //
+    // go to any existing git repo
+    // make some changes and commit them with a message and your CLT
+    // make sure it runs properly
+    // (check for commits under git log)
+    // (you can undo the commit later)
+
 } catch {
     let error = error as! ShellOutError
     print(error.message) // Prints STDERR
